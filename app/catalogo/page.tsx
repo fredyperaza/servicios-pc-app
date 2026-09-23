de262df5e2d79648d2a3fdf82a84dc317939eeb4
FredyperaPg <fredy.peraza@catolica.edu.sv>
2026-09-23 16:17:30 -0600

    Corrige archivo page.tsx vacio en catalogo

---
+++ app/catalogo/page.tsx
@@ -0,0 +1,9 @@
+// app/catalogo/page.tsx
+export default function CatalogoPage() {
+  return (
+    <div className="p-8 text-center">
+      <h1 className="text-2xl font-bold">Catálogo General</h1>
+      <p className="text-slate-600 mt-2">Próximamente más detalles por categoría.</p>
+    </div>
+  );
+}
